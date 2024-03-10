37a272273bef11d83345d41bbff79594a3e3c492
Han Xiao <asher.han.xiao@gmail.com>
2024-03-10 21:04:52 +1100

    feat: create checkout session api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import myUserRoute from "./routes/myUserRoute";
 import myRestaurantRoute from "./routes/myRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 // 1. connect to mongoDB database
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
@@ -33,6 +34,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(1123, () => {
   console.log("server started on port 1123");
